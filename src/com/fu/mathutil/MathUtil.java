b9e18fa5f2bfa9135218403b8e11315f5d2a62b3
jayjaysocool <Haoonn02@gmail.com>
2023-06-02 01:10:44 +0700

    second commit

--- src/com/fu/mathutil/MathUtil.java
+++ src/com/fu/mathutil/MathUtil.java
@@ -13,6 +13,8 @@ public class MathUtil {
         System.out.println("expected:  5! = 120; actual: " + result);
         System.out.println("expected:  6! = 720; actual: " + MathUtility.getFactorial(6));
         System.out.println("expected:  0! = 720; actual: " + MathUtility.getFactorial(0));
+        System.out.println("expected:  1! = 1; actual: " + MathUtility.getFactorial(1));
+        System.out.println("expected:  2! = 2; actual: " + MathUtility.getFactorial(2));
         
     }
     
